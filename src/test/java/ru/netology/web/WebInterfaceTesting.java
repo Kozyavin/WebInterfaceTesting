663312a21bc0b68fb5b1b43adfbc6080599316e5
Kozyavin V <kioci@yandex.ru>
2023-08-19 12:29:45 +0300

    ChangeCode

--- src/test/java/ru/netology/web/WebInterfaceTesting.java
+++ src/test/java/ru/netology/web/WebInterfaceTesting.java
@@ -4,35 +4,56 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebInterfaceTesting {
 
     WebDriver driver;
 
     @BeforeAll
-    public  void setupAll() {
-
+    public void setupAll() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setupUp() {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("-disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-
     }
+
     @AfterEach
     void tearDown() {
         driver.quit();
         driver = null;
     }
-    //@Test
-    //void shouldTestSomething(){
 
-    //}
+    @Test
+    void shouldTestSomething() throws InterruptedException {
+        //Предусловие
+        driver.get("http://localhost:9999");
+        //Выполняемые действия
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Владимир");
+        elements.get(1).sendKeys("+79030000001");
+
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.className("paragraph")).getText();
+        //Проверки
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+    }
 
 }
